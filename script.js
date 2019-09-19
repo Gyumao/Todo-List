cbc76fc5d285691f1ec66a351bbb77e8df9ee10b
Richard Gobert <dev.richard.g@gmail.com>
2019-09-19 17:22:17 +0200

    modification  page js add variable

---
+++ script.js
@@ -0,0 +1,5 @@
+const button = document.getElementById("validation");
+const input = document.getElementById("imputusr");
+const ul = document.querySelector("ul");
+const li = document.createElement("li");
+
